bb6a4e8dad37ec19614e9e86c8ef7c8a19bb1f60
Ivan <ivan.moll@osf.digital>
2022-03-02 12:32:25 +0100

    Create App.js and create Person Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const Person = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h3", { key: "h3" }, props.name),
+        React.createElement("div", { key: "props" }, [
+            Object.keys(props).map(key => React.createElement("div", { key: key }, props[key]))
+        ]),
+    ]);
+};
+
 const App = () => {
-    return ("div", {}, React.createElement("h1", { id: "first-property" }, "Pure React!"));
+    return React.createElement("div", {}, [
+        React.createElement("h1", { key: "h1" }, "Pure React!"),
+        React.createElement(Person, { key: "person", name: "Ivan", age: "33", email: "ivan@react.com" }),
+    ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
