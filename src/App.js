4503b5d36ee07c3c9540ac44eab78ce6ea2df8a2
Ivan <ivan.moll@osf.digital>
2022-03-02 14:13:24 +0100

    Start using JSX instead of createElement()

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Person from './components/Person';
 
+// const App = () => {
+//     return React.createElement("div", {}, [
+//         React.createElement("h1", { key: 'title'}, "Hello World!"),
+//         React.createElement(Person, { key: 'person', name="Ivan" age="33" email="ivan@react.com" }, '')
+//     ]);
+// };
+
 const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", { key: 'title'}, "Hello World!"),
-        React.createElement(Person, { key: 'person' }, '')
-    ]);
+    return (
+        <div>
+            <h1>React with JSX</h1>
+            <Person name="Ivan" age="33" email="ivan@react.com" />
+        </div>
+    );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Person = () => {
-    return React.createElement("h2", {}, "Person component");
+// const Person = () => {
+//     return React.createElement("h2", { id="person-component-title" }, "Person component");
+// };
+
+const Person = (props) => {
+    return (
+        <div>
+            <h2 key="person-component-title">Person component</h2>
+            <ul key="person-component-data">
+                {
+                    Object.keys(props).map(key => (
+                        <li key={key}>
+                            <span>{ key }: </span>
+                            <span>{ props[key] }</span>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
 };
 
 export default Person;
